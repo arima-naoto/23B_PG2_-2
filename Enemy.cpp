df2426a9c2f7936c601bb32356e0c3b394819e75
arima-naoto <k023g1010@g.neec.ac.jp>
2024-02-01 14:22:29 +0900

    敵機を描画しました

---
+++ Enemy.cpp
@@ -0,0 +1,30 @@
+#include "Enemy.h"
+
+void Enemy::Initalize()
+{
+	position_.x = 640;
+	position_.y = 150;
+	radius_ = 20;
+	speed_ = 5;
+	color_ = RED;
+
+	isAlive_ = true;
+}
+
+void Enemy::Update()
+{
+	position_.x += speed_;
+
+	if(position_.x >= 1260 || position_.x <= 20)
+	{
+		speed_ *= -1;
+	}
+}
+
+void Enemy::Draw()
+{
+	if(isAlive_ == true)
+	{
+		Novice::DrawEllipse(position_.x, position_.y, radius_, radius_, 0.0f, color_, kFillModeSolid);
+	}
+}
