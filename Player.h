5865df71adc67d3b531700494e824c666e0d9c1d
arima-naoto <k023g1010@g.neec.ac.jp>
2024-02-01 11:08:35 +0900

    自機を描画しました

---
+++ Player.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Struct.h"
+
+class Player
+{
+public:
+
+	Player(Vector2 position,int radius,int speed,unsigned int color);
+
+	~Player();
+
+	void Update(char *keys);
+
+	void Draw();
+
+private:
+
+	Vector2 position_;
+
+	int radius_;
+
+	int speed_;
+
+	unsigned int color_;
+
+};
