9b5a38ee12faaef103db590c3f55d904cc3dd30c
arima-naoto <k023g1010@g.neec.ac.jp>
2024-02-03 17:57:31 +0900

    自機の弾を描画しました

--- Bullet.h
+++ Bullet.h
@@ -1,5 +1,35 @@
 #pragma once
+#include "struct.h"
+
 class Bullet
 {
+public:
+
+	static bool isShot_;
+
+	Bullet();
+
+	~Bullet();
+
+	void Update();
+
+	void Draw();
+
+	Vector2 GetPos() { return position_; };
+
+	void SetPosition(Vector2 position) { this->position_ = position; };
+
+	int GetRadius() { return radius_; };
+
+private:
+
+	Vector2 position_;
+
+	int radius_;
+
+	int speed_;
+
+	unsigned int color_;
+
 };
 
